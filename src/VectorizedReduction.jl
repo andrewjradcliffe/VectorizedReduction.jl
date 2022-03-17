fbcde176eda6e2b7757f38998c0cf7dd6a03c56d
Andrew Radcliffe <andrew.radcliffe@intel.com>
2022-03-17 06:33:52 -0700

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.26

---
+++ src/VectorizedReduction.jl
@@ -0,0 +1,5 @@
+module VectorizedReduction
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using VectorizedReduction
+using Test
+
+@testset "VectorizedReduction.jl" begin
+    # Write your tests here.
+end
