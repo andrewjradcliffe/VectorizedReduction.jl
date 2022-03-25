b85a8fc3ec735db51553fff228aec31e8bf20ca4
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-03-25 13:04:36 -0700

    Enable anonymous functions

--- src/lvreduce_v3.jl
+++ src/lvreduce_v3.jl
@@ -10,6 +10,12 @@
 
 _dim(::Type{StaticInt{N}}) where {N} = N::Int
 
+# Demonstrated: that technically, `f`, can be anonymous. The reduction still needs
+# to be a regular binary operation which is known to LoopVectorization.
+# Hence, it only seems worthwhile to not force specialization on f.
+# Moreover, it is always a slight advantage to use a named function over an anonymous...
+# In fact, having changed it over, it is a beautiful thing -- same behavior as Julia Base.
+
 function vvmapreduce(f::F, op::OP, init::I, A::AbstractArray{T, N}, dims::NTuple{M, Int}) where {F, OP, I, T, N, M}
     Dᴬ = size(A)
     Dᴮ′ = ntuple(d -> d ∈ dims ? 1 : Dᴬ[d], Val(N))
@@ -62,19 +68,23 @@ vvminimum(A, dims) = vvmapreduce(identity, min, typemax, A, dims)
 
 # reduction over all dims
 @generated function vvmapreduce(f::F, op::OP, init::I, A::AbstractArray{T, N}, ::Colon) where {F, OP, I, T, N}
-    fsym = F.instance
+    # fsym = F.instance
     opsym = OP.instance
     initsym = I.instance
-    Tₒ = Base.promote_op(opsym, Base.promote_op(fsym, T), Int)
+    # Tₒ = Base.promote_op(opsym, Base.promote_op(fsym, T), Int)
     quote
-        ξ = $initsym($Tₒ)
+        # ξ = $initsym($Tₒ)
+        ξ = $initsym(Base.promote_op($opsym, Base.promote_op(f, $T), Int))
         @turbo for i ∈ eachindex(A)
-            ξ = $opsym($fsym(A[i]), ξ)
+            ξ = $opsym(f(A[i]), ξ)
         end
         return ξ
     end
 end
 
+# The dispatch on function type is faster if the function is named,
+# slower (≈ 15%) for anonymous. This would only affect performance in the REPL,
+# hence, it's not really an issue.
 vvsum(f::F, A) where {F<:Function} = vvmapreduce(f, +, zero, A, :)
 vvprod(f::F, A) where {F<:Function} = vvmapreduce(f, *, one, A, :)
 vvmaximum(f::F, A) where {F<:Function} = vvmapreduce(f, max, typemin, A, :)
@@ -159,7 +169,7 @@ vfindall(f::F, A::AbstractArray{T, N}) where {F, T, N} = CartesianIndices(A)[vma
 # Define reduce
 vvreduce(op::OP, init::I, A, dims) where {OP, I} = vvmapreduce(identity, op, init, A, dims)
 
-function staticdim_mapreduce_quote(F, OP, I, static_dims::Vector{Int}, N::Int)
+function staticdim_mapreduce_quote(OP, I, static_dims::Vector{Int}, N::Int)
     A = Expr(:ref, :A, ntuple(d -> Symbol(:i_, d), N)...)
     Bᵥ = Expr(:call, :view, :B)
     Bᵥ′ = Expr(:ref, :Bᵥ)
@@ -204,7 +214,7 @@ function staticdim_mapreduce_quote(F, OP, I, static_dims::Vector{Int}, N::Int)
         end
         # Push to inside innermost loop
         setξ = Expr(:(=), :ξ, Expr(:call, Symbol(OP.instance),
-                                   Expr(:call, Symbol(F.instance), A), :ξ))
+                                   Expr(:call, :f, A), :ξ))
         push!(block.args, setξ)
         setb = Expr(:(=), Bᵥ′, :ξ)
         push!(rblock.args, setb)
@@ -230,7 +240,7 @@ function staticdim_mapreduce_quote(F, OP, I, static_dims::Vector{Int}, N::Int)
         end
         # Push to inside innermost loop
         setξ = Expr(:(=), :ξ, Expr(:call, Symbol(OP.instance),
-                                   Expr(:call, Symbol(F.instance), A), :ξ))
+                                   Expr(:call, :f, A), :ξ))
         push!(block.args, setξ)
         return quote
             Bᵥ = $Bᵥ
@@ -242,7 +252,7 @@ function staticdim_mapreduce_quote(F, OP, I, static_dims::Vector{Int}, N::Int)
     end
 end
 
-function branches_mapreduce_quote(F, OP, I, N::Int, M::Int, D)
+function branches_mapreduce_quote(OP, I, N::Int, M::Int, D)
     static_dims = Int[]
     for m ∈ 1:M
         param = D.parameters[m]
@@ -280,11 +290,11 @@ function branches_mapreduce_quote(F, OP, I, N::Int, M::Int, D)
             return q
         end
     end
-    return staticdim_mapreduce_quote(F, OP, I, static_dims, N)
+    return staticdim_mapreduce_quote(OP, I, static_dims, N)
 end
 
 @generated function _vvmapreduce!(f::F, op::OP, init::I, B::AbstractArray{Tₒ, N}, A::AbstractArray{T, N}, dims::D) where {F, OP, I, Tₒ, T, N, M, D<:Tuple{Vararg{Integer, M}}}
-    branches_mapreduce_quote(F, OP, I, N, M, D)
+    branches_mapreduce_quote(OP, I, N, M, D)
 end
 @generated function _vvmapreduce!(f::F, op::OP, init::I, B::AbstractArray{Tₒ, N}, A::AbstractArray{T, N}, dims::Tuple{}) where {F, OP, I, Tₒ, T, N}
     :(copyto!(B, A); return B)
@@ -303,19 +313,21 @@ end
 
 # reduction over all dims
 @generated function vvmapreduce(f::F, op::OP, init::I, A::AbstractArray{T, N}, ::Colon) where {F, OP, I<:Number, T, N}
-    fsym = F.instance
+    # fsym = F.instance
     opsym = OP.instance
-    Tₒ = Base.promote_op(opsym, Base.promote_op(fsym, T), Int)
+    # Tₒ = Base.promote_op(opsym, Base.promote_op(fsym, T), Int)
     quote
-        ξ = convert($Tₒ, init)
+        # ξ = convert($Tₒ, init)
+        ξ = convert(Base.promote_op($opsym, Base.promote_op(f, $T), Int), init)
         @turbo for i ∈ eachindex(A)
-            ξ = $opsym($fsym(A[i]), ξ)
+            # ξ = $opsym($fsym(A[i]), ξ)
+            ξ = $opsym(f(A[i]), ξ)
         end
         return ξ
     end
 end
 
-function staticdim_mapreduce_init_quote(F, OP, static_dims::Vector{Int}, N::Int)
+function staticdim_mapreduce_init_quote(OP, static_dims::Vector{Int}, N::Int)
     A = Expr(:ref, :A, ntuple(d -> Symbol(:i_, d), N)...)
     Bᵥ = Expr(:call, :view, :B)
     Bᵥ′ = Expr(:ref, :Bᵥ)
@@ -358,7 +370,7 @@ function staticdim_mapreduce_init_quote(F, OP, static_dims::Vector{Int}, N::Int)
         end
         # Push to inside innermost loop
         setξ = Expr(:(=), :ξ, Expr(:call, Symbol(OP.instance),
-                                   Expr(:call, Symbol(F.instance), A), :ξ))
+                                   Expr(:call, :f, A), :ξ))
         push!(block.args, setξ)
         setb = Expr(:(=), Bᵥ′, :ξ)
         push!(rblock.args, setb)
@@ -383,7 +395,7 @@ function staticdim_mapreduce_init_quote(F, OP, static_dims::Vector{Int}, N::Int)
         end
         # Push to inside innermost loop
         setξ = Expr(:(=), :ξ, Expr(:call, Symbol(OP.instance),
-                                   Expr(:call, Symbol(F.instance), A), :ξ))
+                                   Expr(:call, :f, A), :ξ))
         push!(block.args, setξ)
         return quote
             Bᵥ = $Bᵥ
@@ -395,7 +407,7 @@ function staticdim_mapreduce_init_quote(F, OP, static_dims::Vector{Int}, N::Int)
     end
 end
 
-function branches_mapreduce_init_quote(F, OP, N::Int, M::Int, D)
+function branches_mapreduce_init_quote(OP, N::Int, M::Int, D)
     static_dims = Int[]
     for m ∈ 1:M
         param = D.parameters[m]
@@ -433,11 +445,11 @@ function branches_mapreduce_init_quote(F, OP, N::Int, M::Int, D)
             return q
         end
     end
-    return staticdim_mapreduce_init_quote(F, OP, static_dims, N)
+    return staticdim_mapreduce_init_quote(OP, static_dims, N)
 end
 
 @generated function _vvmapreduce_init!(f::F, op::OP, init::I, B::AbstractArray{Tₒ, N}, A::AbstractArray{T, N}, dims::D) where {F, OP, I, Tₒ, T, N, M, D<:Tuple{Vararg{Integer, M}}}
-    branches_mapreduce_init_quote(F, OP, N, M, D)
+    branches_mapreduce_init_quote(OP, N, M, D)
 end
 @generated function _vvmapreduce_init!(f::F, op::OP, init::I, B::AbstractArray{Tₒ, N}, A::AbstractArray{T, N}, dims::Tuple{}) where {F, OP, I, Tₒ, T, N}
     :(copyto!(B, A); return B)
@@ -445,145 +457,156 @@ end
 
 
 ################
-# Demonstrates that technically, `f`, can be anonymous. The reduction still needs
-# to be a regular binary operation which is known to LoopVectorization.
-# Hence, it only seems worthwhile to not force specialization on f.
-# Moreover, it is always a slight advantage to use a named function over an anonymous...
-function vvmapreduce2(f::F, op::OP, init::I, A::AbstractArray{T, N}, dims::NTuple{M, Int}) where {F, OP, I, T, N, M}
-    Dᴬ = size(A)
-    Dᴮ′ = ntuple(d -> d ∈ dims ? 1 : Dᴬ[d], Val(N))
-    B = similar(A, Base.promote_op(op, Base.promote_op(f, T), Int), Dᴮ′)
-    _vvmapreduce2!(f, op, init, B, A, dims)
-    return B
-end
+# function vvmapreduce2(f::F, op::OP, init::I, A::AbstractArray{T, N}, dims::NTuple{M, Int}) where {F, OP, I, T, N, M}
+#     Dᴬ = size(A)
+#     Dᴮ′ = ntuple(d -> d ∈ dims ? 1 : Dᴬ[d], Val(N))
+#     B = similar(A, Base.promote_op(op, Base.promote_op(f, T), Int), Dᴮ′)
+#     _vvmapreduce2!(f, op, init, B, A, dims)
+#     return B
+# end
+# # reduction over all dims
+# @generated function vvmapreduce2(f::F, op::OP, init::I, A::AbstractArray{T, N}, ::Colon) where {F, OP, I, T, N}
+#     fsym = F.instance
+#     opsym = OP.instance
+#     initsym = I.instance
+#     # Tₒ = Base.promote_op(opsym, Base.promote_op(fsym, T), Int)
+#     quote
+#         # ξ = $initsym($Tₒ)
+#         ξ = $initsym(Base.promote_op($opsym, Base.promote_op(f, $T), Int))
+#         @turbo for i ∈ eachindex(A)
+#             ξ = $opsym(f(A[i]), ξ)
+#         end
+#         return ξ
+#     end
+# end
 
-function staticdim_mapreduce2_quote(I, static_dims::Vector{Int}, N::Int)
-    A = Expr(:ref, :A, ntuple(d -> Symbol(:i_, d), N)...)
-    Bᵥ = Expr(:call, :view, :B)
-    Bᵥ′ = Expr(:ref, :Bᵥ)
-    rinds = Int[]
-    nrinds = Int[]
-    for d = 1:N
-        if d ∈ static_dims
-            push!(Bᵥ.args, Expr(:call, :firstindex, :B, d))
-            push!(rinds, d)
-        else
-            push!(Bᵥ.args, :)
-            push!(nrinds, d)
-            push!(Bᵥ′.args, Symbol(:i_, d))
-        end
-    end
-    reverse!(rinds)
-    reverse!(nrinds)
-    if !isempty(nrinds)
-        block = Expr(:block)
-        loops = Expr(:for, Expr(:(=), Symbol(:i_, nrinds[1]),
-                                Expr(:call, :indices, Expr(:tuple, :A, :B), nrinds[1])), block)
-        # loops = Expr(:for, :($(Symbol(:i_, nrinds[1])) = indices((A, B), $(nrinds[1]))), block)
-        for i = 2:length(nrinds)
-            # for d ∈ @view(nrinds[2:end])
-            newblock = Expr(:block)
-            push!(block.args,
-                  Expr(:for, Expr(:(=), Symbol(:i_, nrinds[i]),
-                                  Expr(:call, :indices, Expr(:tuple, :A, :B), nrinds[i])), newblock))
-            # push!(block.args, Expr(:for, :($(Symbol(:i_, d)) = indices((A, B), $d)), newblock))
-            block = newblock
-        end
-        rblock = block
-        # Pre-reduction
-        ξ = Expr(:(=), :ξ, Expr(:call, Symbol(I.instance), Expr(:call, :eltype, :Bᵥ)))
-        push!(rblock.args, ξ)
-        # Reduction loop
-        for d ∈ rinds
-            newblock = Expr(:block)
-            push!(block.args, Expr(:for, Expr(:(=), Symbol(:i_, d), Expr(:call, :axes, :A, d)), newblock))
-            # push!(block.args, Expr(:for, :($(Symbol(:i_, d)) = axes(A, $d)), newblock))
-            block = newblock
-        end
-        # Push to inside innermost loop
-        setξ = Expr(:(=), :ξ, Expr(:call, :op,
-                                   Expr(:call, :f, A), :ξ))
-        push!(block.args, setξ)
-        setb = Expr(:(=), Bᵥ′, :ξ)
-        push!(rblock.args, setb)
-        return quote
-            Bᵥ = $Bᵥ
-            @turbo $loops
-            return B
-        end
-    else
-        # Pre-reduction
-        ξ = Expr(:(=), :ξ, Expr(:call, Symbol(I.instance), Expr(:call, :eltype, :Bᵥ)))
-        # Reduction loop
-        block = Expr(:block)
-        loops = Expr(:for, Expr(:(=), Symbol(:i_, rinds[1]),
-                                Expr(:call, :axes, :A, rinds[1])), block)
-        # loops = Expr(:for, :($(Symbol(:i_, rinds[1])) = axes(A, $(rinds[1]))), block)
-        for i = 2:length(rinds)
-            newblock = Expr(:block)
-            push!(block.args, Expr(:for, Expr(:(=), Symbol(:i_, rinds[i]),
-                                              Expr(:call, :axes, :A, rinds[i])), newblock))
-            # push!(block.args, Expr(:for, :($(Symbol(:i_, d)) = axes(A, $d)), newblock))
-            block = newblock
-        end
-        # Push to inside innermost loop
-        setξ = Expr(:(=), :ξ, Expr(:call, :op,
-                                   Expr(:call, :f, A), :ξ))
-        push!(block.args, setξ)
-        return quote
-            Bᵥ = $Bᵥ
-            $ξ
-            @turbo $loops
-            Bᵥ[] = ξ
-            return B
-        end
-    end
-end
+# function staticdim_mapreduce2_quote(OP, I, static_dims::Vector{Int}, N::Int)
+#     A = Expr(:ref, :A, ntuple(d -> Symbol(:i_, d), N)...)
+#     Bᵥ = Expr(:call, :view, :B)
+#     Bᵥ′ = Expr(:ref, :Bᵥ)
+#     rinds = Int[]
+#     nrinds = Int[]
+#     for d = 1:N
+#         if d ∈ static_dims
+#             push!(Bᵥ.args, Expr(:call, :firstindex, :B, d))
+#             push!(rinds, d)
+#         else
+#             push!(Bᵥ.args, :)
+#             push!(nrinds, d)
+#             push!(Bᵥ′.args, Symbol(:i_, d))
+#         end
+#     end
+#     reverse!(rinds)
+#     reverse!(nrinds)
+#     if !isempty(nrinds)
+#         block = Expr(:block)
+#         loops = Expr(:for, Expr(:(=), Symbol(:i_, nrinds[1]),
+#                                 Expr(:call, :indices, Expr(:tuple, :A, :B), nrinds[1])), block)
+#         # loops = Expr(:for, :($(Symbol(:i_, nrinds[1])) = indices((A, B), $(nrinds[1]))), block)
+#         for i = 2:length(nrinds)
+#             # for d ∈ @view(nrinds[2:end])
+#             newblock = Expr(:block)
+#             push!(block.args,
+#                   Expr(:for, Expr(:(=), Symbol(:i_, nrinds[i]),
+#                                   Expr(:call, :indices, Expr(:tuple, :A, :B), nrinds[i])), newblock))
+#             # push!(block.args, Expr(:for, :($(Symbol(:i_, d)) = indices((A, B), $d)), newblock))
+#             block = newblock
+#         end
+#         rblock = block
+#         # Pre-reduction
+#         ξ = Expr(:(=), :ξ, Expr(:call, Symbol(I.instance), Expr(:call, :eltype, :Bᵥ)))
+#         push!(rblock.args, ξ)
+#         # Reduction loop
+#         for d ∈ rinds
+#             newblock = Expr(:block)
+#             push!(block.args, Expr(:for, Expr(:(=), Symbol(:i_, d), Expr(:call, :axes, :A, d)), newblock))
+#             # push!(block.args, Expr(:for, :($(Symbol(:i_, d)) = axes(A, $d)), newblock))
+#             block = newblock
+#         end
+#         # Push to inside innermost loop
+#         setξ = Expr(:(=), :ξ, Expr(:call, Symbol(OP.instance),
+#                                    Expr(:call, :f, A), :ξ))
+#         push!(block.args, setξ)
+#         setb = Expr(:(=), Bᵥ′, :ξ)
+#         push!(rblock.args, setb)
+#         return quote
+#             Bᵥ = $Bᵥ
+#             @turbo $loops
+#             return B
+#         end
+#     else
+#         # Pre-reduction
+#         ξ = Expr(:(=), :ξ, Expr(:call, Symbol(I.instance), Expr(:call, :eltype, :Bᵥ)))
+#         # Reduction loop
+#         block = Expr(:block)
+#         loops = Expr(:for, Expr(:(=), Symbol(:i_, rinds[1]),
+#                                 Expr(:call, :axes, :A, rinds[1])), block)
+#         # loops = Expr(:for, :($(Symbol(:i_, rinds[1])) = axes(A, $(rinds[1]))), block)
+#         for i = 2:length(rinds)
+#             newblock = Expr(:block)
+#             push!(block.args, Expr(:for, Expr(:(=), Symbol(:i_, rinds[i]),
+#                                               Expr(:call, :axes, :A, rinds[i])), newblock))
+#             # push!(block.args, Expr(:for, :($(Symbol(:i_, d)) = axes(A, $d)), newblock))
+#             block = newblock
+#         end
+#         # Push to inside innermost loop
+#         setξ = Expr(:(=), :ξ, Expr(:call, Symbol(OP.instance),
+#                                    Expr(:call, :f, A), :ξ))
+#         push!(block.args, setξ)
+#         return quote
+#             Bᵥ = $Bᵥ
+#             $ξ
+#             @turbo $loops
+#             Bᵥ[] = ξ
+#             return B
+#         end
+#     end
+# end
 
-function branches_mapreduce2_quote(I, N::Int, M::Int, D)
-    static_dims = Int[]
-    for m ∈ 1:M
-        param = D.parameters[m]
-        if param <: StaticInt
-            new_dim = _dim(param)::Int
-            push!(static_dims, new_dim)
-        else
-            # tuple of static dimensions
-            t = Expr(:tuple)
-            for n ∈ static_dims
-                push!(t.args, :(StaticInt{$n}()))
-            end
-            q = Expr(:block, :(dimm = dims[$m]))
-            qold = q
-            # if-elseif statements
-            ifsym = :if
-            for n ∈ 1:N
-                n ∈ static_dims && continue
-                tc = copy(t)
-                push!(tc.args, :(StaticInt{$n}()))
-                qnew = Expr(ifsym, :(dimm == $n), :(return _vvmapreduce2!(f, op, init, B, A, $tc)))
-                for r ∈ m+1:M
-                    push!(tc.args, :(dims[$r]))
-                end
-                push!(qold.args, qnew)
-                qold = qnew
-                ifsym = :elseif
-            end
-            # else statement
-            tc = copy(t)
-            for r ∈ m+1:M
-                push!(tc.args, :(dims[$r]))
-            end
-            push!(qold.args, Expr(:block, :(return _vvmapreduce2!(f, op, init, B, A, $tc))))
-            return q
-        end
-    end
-    return staticdim_mapreduce2_quote(I, static_dims, N)
-end
+# function branches_mapreduce2_quote(OP, I, N::Int, M::Int, D)
+#     static_dims = Int[]
+#     for m ∈ 1:M
+#         param = D.parameters[m]
+#         if param <: StaticInt
+#             new_dim = _dim(param)::Int
+#             push!(static_dims, new_dim)
+#         else
+#             # tuple of static dimensions
+#             t = Expr(:tuple)
+#             for n ∈ static_dims
+#                 push!(t.args, :(StaticInt{$n}()))
+#             end
+#             q = Expr(:block, :(dimm = dims[$m]))
+#             qold = q
+#             # if-elseif statements
+#             ifsym = :if
+#             for n ∈ 1:N
+#                 n ∈ static_dims && continue
+#                 tc = copy(t)
+#                 push!(tc.args, :(StaticInt{$n}()))
+#                 qnew = Expr(ifsym, :(dimm == $n), :(return _vvmapreduce2!(f, op, init, B, A, $tc)))
+#                 for r ∈ m+1:M
+#                     push!(tc.args, :(dims[$r]))
+#                 end
+#                 push!(qold.args, qnew)
+#                 qold = qnew
+#                 ifsym = :elseif
+#             end
+#             # else statement
+#             tc = copy(t)
+#             for r ∈ m+1:M
+#                 push!(tc.args, :(dims[$r]))
+#             end
+#             push!(qold.args, Expr(:block, :(return _vvmapreduce2!(f, op, init, B, A, $tc))))
+#             return q
+#         end
+#     end
+#     return staticdim_mapreduce2_quote(OP, I, static_dims, N)
+# end
 
-@generated function _vvmapreduce2!(f::F, op::OP, init::I, B::AbstractArray{Tₒ, N}, A::AbstractArray{T, N}, dims::D) where {F, OP, I, Tₒ, T, N, M, D<:Tuple{Vararg{Integer, M}}}
-    branches_mapreduce2_quote(I, N, M, D)
-end
-@generated function _vvmapreduce2!(f::F, op::OP, init::I, B::AbstractArray{Tₒ, N}, A::AbstractArray{T, N}, dims::Tuple{}) where {F, OP, I, Tₒ, T, N}
-    :(copyto!(B, A); return B)
-end
+# @generated function _vvmapreduce2!(f::F, op::OP, init::I, B::AbstractArray{Tₒ, N}, A::AbstractArray{T, N}, dims::D) where {F, OP, I, Tₒ, T, N, M, D<:Tuple{Vararg{Integer, M}}}
+#     branches_mapreduce2_quote(OP, I, N, M, D)
+# end
+# @generated function _vvmapreduce2!(f::F, op::OP, init::I, B::AbstractArray{Tₒ, N}, A::AbstractArray{T, N}, dims::Tuple{}) where {F, OP, I, Tₒ, T, N}
+#     :(copyto!(B, A); return B)
+# end
