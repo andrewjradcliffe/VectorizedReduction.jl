873b1b526c5356fef4c76caacbce83ada0e60176
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-03-29 12:42:08 -0700

    Add vtmapreduce

--- src/vmapreduce.jl
+++ src/vmapreduce.jl
@@ -8,6 +8,8 @@
 # - Using compile-time branch resolution rather than dispatch system (well,
 #   less on the dispatch system)
 _dim(::Type{StaticInt{N}}) where {N} = N::Int
+# Stable return type version, otherwise it's just a suggestion
+# _dim(::Type{StaticInt{N}})::Int where {N} = N
 
 # Demonstrated: that technically, `f`, can be anonymous. The reduction still needs
 # to be a regular binary operation which is known to LoopVectorization.
@@ -547,3 +549,329 @@ end
 # @generated function _vvmapreduce2!(f::F, op::OP, init::I, B::AbstractArray{Tₒ, N}, A::AbstractArray{T, N}, dims::Tuple{}) where {F, OP, I, Tₒ, T, N}
 #     :(copyto!(B, A); return B)
 # end
+
+############################################################################################
+function vtmapreduce(f::F, op::OP, init::I, A::AbstractArray{T, N}, dims::NTuple{M, Int}) where {F, OP, I, T, N, M}
+    Dᴬ = size(A)
+    Dᴮ′ = ntuple(d -> d ∈ dims ? 1 : Dᴬ[d], Val(N))
+    B = similar(A, Base.promote_op(op, Base.promote_op(f, T), Int), Dᴮ′)
+    _vtmapreduce!(f, op, init, B, A, dims)
+    return B
+end
+# vtmapreduce(f, op, init, A, dims::Int) = vtmapreduce(f, op, init, A, (dims,))
+
+# dims determination would ideally be non-allocating. Also, who would
+# call this anyway? Almost assuredly, a caller would already know dims, hence
+# just call _vtmapreduce! anyway.
+function vtmapreduce!(f::F, op::OP, init::I, B::AbstractArray{Tₒ, N}, A::AbstractArray{T, N}) where {F, OP, I, Tₒ, T, N}
+    Dᴬ = size(A)
+    Dᴮ = size(B)
+    dims = Tuple((d for d ∈ eachindex(Dᴮ) if isone(Dᴮ[d])))
+    all(d -> Dᴮ[d] == Dᴬ[d], (d for d ∈ eachindex(Dᴮ) if !isone(Dᴮ[d]))) || throw(DimensionMismatch)
+    _vtmapreduce!(f, op, init, B, A, dims)
+    return B
+end
+
+# Convenience definitions
+vtsum(f::F, A, dims) where {F} = vtmapreduce(f, +, zero, A, dims)
+vtprod(f::F, A, dims) where {F} = vtmapreduce(f, *, one, A, dims)
+vtmaximum(f::F, A, dims) where {F} = vtmapreduce(f, max, typemin, A, dims)
+vtminimum(f::F, A, dims) where {F} = vtmapreduce(f, min, typemax, A, dims)
+
+vtsum(A, dims) = vtmapreduce(identity, +, zero, A, dims)
+vtprod(A, dims) = vtmapreduce(identity, *, one, A, dims)
+vtmaximum(A, dims) = vtmapreduce(identity, max, typemin, A, dims)
+vtminimum(A, dims) = vtmapreduce(identity, min, typemax, A, dims)
+
+# reduction over all dims
+@generated function vtmapreduce(f::F, op::OP, init::I, A::AbstractArray{T, N}, ::Colon) where {F, OP, I, T, N}
+    opsym = OP.instance
+    initsym = I.instance
+    quote
+        ξ = $initsym(Base.promote_op($opsym, Base.promote_op(f, $T), Int))
+        @tturbo for i ∈ eachindex(A)
+            ξ = $opsym(f(A[i]), ξ)
+        end
+        return ξ
+    end
+end
+
+vtsum(f::F, A) where {F<:Function} = vtmapreduce(f, +, zero, A, :)
+vtprod(f::F, A) where {F<:Function} = vtmapreduce(f, *, one, A, :)
+vtmaximum(f::F, A) where {F<:Function} = vtmapreduce(f, max, typemin, A, :)
+vtminimum(f::F, A) where {F<:Function} = vtmapreduce(f, min, typemax, A, :)
+
+vtsum(A) = vtmapreduce(identity, +, zero, A, :)
+vtprod(A) = vtmapreduce(identity, *, one, A, :)
+vtmaximum(A) = vtmapreduce(identity, max, typemin, A, :)
+vtminimum(A) = vtmapreduce(identity, min, typemax, A, :)
+
+vtextrema(f::F, A, dims) where {F} = collect(zip(vtminimum(f, A, dims), vtmaximum(f, A, dims)))
+vtextrema(f::F, A, ::Colon) where {F} = (vtminimum(f, A, :), vtmaximum(f, A, :))
+vtextrema(f::F, A) where {F<:Function} = vtextrema(f, A, :)
+vtextrema(A, dims) = vtextrema(identity, A, dims)
+vtextrema(A) = (vtminimum(A), vtmaximum(A))
+
+vtreduce(op::OP, init::I, A, dims) where {OP, I} = vtmapreduce(identity, op, init, A, dims)
+
+function staticdim_tmapreduce_quote(OP, I, static_dims::Vector{Int}, N::Int)
+    A = Expr(:ref, :A, ntuple(d -> Symbol(:i_, d), N)...)
+    Bᵥ = Expr(:call, :view, :B)
+    Bᵥ′ = Expr(:ref, :Bᵥ)
+    rinds = Int[]
+    nrinds = Int[]
+    for d = 1:N
+        if d ∈ static_dims
+            push!(Bᵥ.args, Expr(:call, :firstindex, :B, d))
+            push!(rinds, d)
+        else
+            push!(Bᵥ.args, :)
+            push!(nrinds, d)
+            push!(Bᵥ′.args, Symbol(:i_, d))
+        end
+    end
+    reverse!(rinds)
+    reverse!(nrinds)
+    if !isempty(nrinds)
+        block = Expr(:block)
+        loops = Expr(:for, :($(Symbol(:i_, nrinds[1])) = indices((A, B), $(nrinds[1]))), block)
+        for d ∈ @view(nrinds[2:end])
+            newblock = Expr(:block)
+            push!(block.args, Expr(:for, :($(Symbol(:i_, d)) = indices((A, B), $d)), newblock))
+            block = newblock
+        end
+        rblock = block
+        # Pre-reduction
+        ξ = Expr(:(=), :ξ, Expr(:call, Symbol(I.instance), Expr(:call, :eltype, :Bᵥ)))
+        push!(rblock.args, ξ)
+        # Reduction loop
+        for d ∈ rinds
+            newblock = Expr(:block)
+            push!(block.args, Expr(:for, :($(Symbol(:i_, d)) = axes(A, $d)), newblock))
+            block = newblock
+        end
+        # Push to inside innermost loop
+        setξ = Expr(:(=), :ξ, Expr(:call, Symbol(OP.instance), Expr(:call, :f, A), :ξ))
+        push!(block.args, setξ)
+        setb = Expr(:(=), Bᵥ′, :ξ)
+        push!(rblock.args, setb)
+        return quote
+            Bᵥ = $Bᵥ
+            @tturbo $loops
+            return B
+        end
+    else
+        # Pre-reduction
+        ξ = Expr(:(=), :ξ, Expr(:call, Symbol(I.instance), Expr(:call, :eltype, :Bᵥ)))
+        # Reduction loop
+        block = Expr(:block)
+        loops = Expr(:for, :($(Symbol(:i_, rinds[1])) = axes(A, $(rinds[1]))), block)
+        for i = 2:length(rinds)
+            newblock = Expr(:block)
+            push!(block.args, Expr(:for, :($(Symbol(:i_, d)) = axes(A, $d)), newblock))
+            block = newblock
+        end
+        # Push to inside innermost loop
+        setξ = Expr(:(=), :ξ, Expr(:call, Symbol(OP.instance), Expr(:call, :f, A), :ξ))
+        push!(block.args, setξ)
+        return quote
+            Bᵥ = $Bᵥ
+            $ξ
+            @tturbo $loops
+            Bᵥ[] = ξ
+            return B
+        end
+    end
+end
+
+function branches_tmapreduce_quote(OP, I, N::Int, M::Int, D)
+    static_dims = Int[]
+    for m ∈ 1:M
+        param = D.parameters[m]
+        if param <: StaticInt
+            new_dim = _dim(param)::Int
+            push!(static_dims, new_dim)
+        else
+            # tuple of static dimensions
+            t = Expr(:tuple)
+            for n ∈ static_dims
+                push!(t.args, :(StaticInt{$n}()))
+            end
+            q = Expr(:block, :(dimm = dims[$m]))
+            qold = q
+            # if-elseif statements
+            ifsym = :if
+            for n ∈ 1:N
+                n ∈ static_dims && continue
+                tc = copy(t)
+                push!(tc.args, :(StaticInt{$n}()))
+                qnew = Expr(ifsym, :(dimm == $n), :(return _vtmapreduce!(f, op, init, B, A, $tc)))
+                for r ∈ m+1:M
+                    push!(tc.args, :(dims[$r]))
+                end
+                push!(qold.args, qnew)
+                qold = qnew
+                ifsym = :elseif
+            end
+            # else statement
+            tc = copy(t)
+            for r ∈ m+1:M
+                push!(tc.args, :(dims[$r]))
+            end
+            push!(qold.args, Expr(:block, :(return _vtmapreduce!(f, op, init, B, A, $tc))))
+            return q
+        end
+    end
+    return staticdim_tmapreduce_quote(OP, I, static_dims, N)
+end
+
+@generated function _vtmapreduce!(f::F, op::OP, init::I, B::AbstractArray{Tₒ, N}, A::AbstractArray{T, N}, dims::D) where {F, OP, I, Tₒ, T, N, M, D<:Tuple{Vararg{Integer, M}}}
+    branches_tmapreduce_quote(OP, I, N, M, D)
+end
+@generated function _vtmapreduce!(f::F, op::OP, init::I, B::AbstractArray{Tₒ, N}, A::AbstractArray{T, N}, dims::Tuple{}) where {F, OP, I, Tₒ, T, N}
+    :(copyto!(B, A); return B)
+end
+
+################
+# Version wherein an initial value is supplied
+
+function vtmapreduce(f::F, op::OP, init::I, A::AbstractArray{T, N}, dims::NTuple{M, Int}) where {F, OP, I<:Number, T, N, M}
+    Dᴬ = size(A)
+    Dᴮ′ = ntuple(d -> d ∈ dims ? 1 : Dᴬ[d], Val(N))
+    B = similar(A, Base.promote_op(op, Base.promote_op(f, T), Int), Dᴮ′)
+    _vtmapreduce_init!(f, op, init, B, A, dims)
+    return B
+end
+
+# reduction over all dims
+@generated function vtmapreduce(f::F, op::OP, init::I, A::AbstractArray{T, N}, ::Colon) where {F, OP, I<:Number, T, N}
+    opsym = OP.instance
+    quote
+        ξ = convert(Base.promote_op($opsym, Base.promote_op(f, $T), Int), init)
+        @tturbo for i ∈ eachindex(A)
+            ξ = $opsym(f(A[i]), ξ)
+        end
+        return ξ
+    end
+end
+
+function staticdim_tmapreduce_init_quote(OP, static_dims::Vector{Int}, N::Int)
+    A = Expr(:ref, :A, ntuple(d -> Symbol(:i_, d), N)...)
+    Bᵥ = Expr(:call, :view, :B)
+    Bᵥ′ = Expr(:ref, :Bᵥ)
+    rinds = Int[]
+    nrinds = Int[]
+    for d = 1:N
+        if d ∈ static_dims
+            push!(Bᵥ.args, Expr(:call, :firstindex, :B, d))
+            push!(rinds, d)
+        else
+            push!(Bᵥ.args, :)
+            push!(nrinds, d)
+            push!(Bᵥ′.args, Symbol(:i_, d))
+        end
+    end
+    reverse!(rinds)
+    reverse!(nrinds)
+    if !isempty(nrinds)
+        ξ₀ = Expr(:call, :convert, Expr(:call, :eltype, :Bᵥ), :init)
+        block = Expr(:block)
+        loops = Expr(:for, :($(Symbol(:i_, nrinds[1])) = indices((A, B), $(nrinds[1]))), block)
+        for d ∈ @view(nrinds[2:end])
+            newblock = Expr(:block)
+            push!(block.args, Expr(:for, :($(Symbol(:i_, d)) = indices((A, B), $d)), newblock))
+            block = newblock
+        end
+        rblock = block
+        # Pre-reduction
+        ξ = Expr(:(=), :ξ, :ξ₀)
+        push!(rblock.args, ξ)
+        # Reduction loop
+        for d ∈ rinds
+            newblock = Expr(:block)
+            push!(block.args, Expr(:for, :($(Symbol(:i_, d)) = axes(A, $d)), newblock))
+            block = newblock
+        end
+        # Push to inside innermost loop
+        setξ = Expr(:(=), :ξ, Expr(:call, Symbol(OP.instance), Expr(:call, :f, A), :ξ))
+        push!(block.args, setξ)
+        setb = Expr(:(=), Bᵥ′, :ξ)
+        push!(rblock.args, setb)
+        return quote
+            Bᵥ = $Bᵥ
+            ξ₀ = $ξ₀
+            @tturbo $loops
+            return B
+        end
+    else
+        # Pre-reduction
+        ξ = Expr(:(=), :ξ, Expr(:call, Expr(:call, :eltype, :Bᵥ), :init))
+        # Reduction loop
+        block = Expr(:block)
+        loops = Expr(:for, :($(Symbol(:i_, rinds[1])) = axes(A, $(rinds[1]))), block)
+        # for i = 2:length(rinds)
+        for d ∈ @view(rinds[2:end])
+            newblock = Expr(:block)
+            push!(block.args, Expr(:for, :($(Symbol(:i_, d)) = axes(A, $d)), newblock))
+            block = newblock
+        end
+        # Push to inside innermost loop
+        setξ = Expr(:(=), :ξ, Expr(:call, Symbol(OP.instance), Expr(:call, :f, A), :ξ))
+        push!(block.args, setξ)
+        return quote
+            Bᵥ = $Bᵥ
+            $ξ
+            @tturbo $loops
+            Bᵥ[] = ξ
+            return B
+        end
+    end
+end
+
+function branches_tmapreduce_init_quote(OP, N::Int, M::Int, D)
+    static_dims = Int[]
+    for m ∈ 1:M
+        param = D.parameters[m]
+        if param <: StaticInt
+            new_dim = _dim(param)::Int
+            push!(static_dims, new_dim)
+        else
+            # tuple of static dimensions
+            t = Expr(:tuple)
+            for n ∈ static_dims
+                push!(t.args, :(StaticInt{$n}()))
+            end
+            q = Expr(:block, :(dimm = dims[$m]))
+            qold = q
+            # if-elseif statements
+            ifsym = :if
+            for n ∈ 1:N
+                n ∈ static_dims && continue
+                tc = copy(t)
+                push!(tc.args, :(StaticInt{$n}()))
+                qnew = Expr(ifsym, :(dimm == $n), :(return _vtmapreduce_init!(f, op, init, B, A, $tc)))
+                for r ∈ m+1:M
+                    push!(tc.args, :(dims[$r]))
+                end
+                push!(qold.args, qnew)
+                qold = qnew
+                ifsym = :elseif
+            end
+            # else statement
+            tc = copy(t)
+            for r ∈ m+1:M
+                push!(tc.args, :(dims[$r]))
+            end
+            push!(qold.args, Expr(:block, :(return _vtmapreduce_init!(f, op, init, B, A, $tc))))
+            return q
+        end
+    end
+    return staticdim_tmapreduce_init_quote(OP, static_dims, N)
+end
+
+@generated function _vtmapreduce_init!(f::F, op::OP, init::I, B::AbstractArray{Tₒ, N}, A::AbstractArray{T, N}, dims::D) where {F, OP, I, Tₒ, T, N, M, D<:Tuple{Vararg{Integer, M}}}
+    branches_tmapreduce_init_quote(OP, N, M, D)
+end
+@generated function _vtmapreduce_init!(f::F, op::OP, init::I, B::AbstractArray{Tₒ, N}, A::AbstractArray{T, N}, dims::Tuple{}) where {F, OP, I, Tₒ, T, N}
+    :(copyto!(B, A); return B)
+end
