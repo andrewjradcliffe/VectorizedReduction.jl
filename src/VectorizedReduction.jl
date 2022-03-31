d0601b95054498e09818ac7d9a1b5802fcddca51
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-03-30 18:04:06 -0700

    Define an export list

--- src/VectorizedReduction.jl
+++ src/VectorizedReduction.jl
@@ -1,5 +1,26 @@
 module VectorizedReduction
 
-# Write your package code here.
+export vvmapreduce, vvmapreduce!, vvsum, vvprod, vvmaximum, vvminimum, vvextrema,
+    vtmapreduce, vtmapreduce!, vtsum, vtprod, vtmaximum, vtminimum, vtextrema
+
+include("vmapreduce.jl")
+
+export vfindmin, vfindmax, vargmin, vargmax,
+    vtfindmin, vtfindmax, vtargmin, vtargmax
+
+include("vfindminmax.jl")
+include("vargminmax.jl")
+
+export vlogsumexp, vtlogsumexp
+
+include("vlogsumexp.jl")
+
+export vlogsoftmax, vtlogsoftmax
+
+include("vlogsoftmax.jl")
+
+export vsoftmax, vtsoftmax
+
+include("vsoftmax.jl")
 
 end
