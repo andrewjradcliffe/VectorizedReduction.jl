16af262f69b9a2d283d24ed8adce1c73afda48be
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-04-14 17:31:44 -0700

    Add usage recommendation

--- src/vboolean.jl
+++ src/vboolean.jl
@@ -68,6 +68,15 @@ Determine whether predicate `p` returns true for any elements over the given `di
 If `p` is omitted, test whether any values along the given `dims` are `true`
 (in which case `A` should be `AbstractArray{Bool}`).
 
+# Usage Recommendation
+If `A` is reasonably small, `vany` may be faster than `any`; however, as the size
+of `A` grows, the probability of any element returning true inevitably increases
+(it is repeated Bernoulli sampling, thus, even with a very small success probability,
+ a large number of tries makes may yield a scenario where the `break` of `any` wins out).
+Consequently, the probability of individual elements being `true` should determine choice --
+if one suspects a reasonable success probability, then `any` may be preferable, depending
+on the size `A`. More testing is needed to determine potential breakpoints.
+
 # Additional Notes
 This function suffers from the same issue as `vfindmax` and friends -- reductions
 which include the first dimension with zero masks are not yet supported by LoopVectorization.
