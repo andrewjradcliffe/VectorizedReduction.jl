0bcbbf7590722887ac906f94706a257336a4413f
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-04-14 17:35:13 -0700

    Add note for vall

--- src/vboolean.jl
+++ src/vboolean.jl
@@ -70,7 +70,7 @@ If `p` is omitted, test whether any values along the given `dims` are `true`
 
 # Usage Recommendation
 If `A` is reasonably small, `vany` may be faster than `any`; however, as the size
-of `A` grows, the probability of any element returning true inevitably increases
+of `A` grows, the probability of any element returning `true` inevitably increases
 (it is repeated Bernoulli sampling, thus, even with a very small success probability,
  a large number of tries makes may yield a scenario where the `break` of `any` wins out).
 Consequently, the probability of individual elements being `true` should determine choice --
@@ -98,6 +98,15 @@ vany(f, A, dims::Int) = vany(f, A, (dims,))
 Determine whether predicate `p` returns true for all elements over the given `dims`.
 If `p` is omitted, test whether all values along the given `dims` are `true`
 (in which case `A` should be `AbstractArray{Bool}`).
+
+# Usage Recommendation
+If `A` is reasonably small, `vall` may be faster than `all`; however, as the size
+of `A` grows, the probability of all element returning `false` inevitably increases
+(it is repeated Bernoulli sampling, thus, even with a very small success probability,
+ a large number of tries makes may yield a scenario where the `break` of `all` wins out).
+Consequently, the probability of individual elements being `true` should determine choice --
+if one suspects a reasonable success probability, then `all` may be preferable, depending
+on the size `A`. More testing is needed to determine potential breakpoints.
 
 # Additional Notes
 This function suffers from the same issue as `vfindmax` and friends -- reductions
