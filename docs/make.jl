7e1e74883cc5ffa17d36b9f9d8e197d7bb0a69b9
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-04-18 17:24:42 -0700

    Begin work on proper (non-README) documentation

---
+++ docs/make.jl
@@ -0,0 +1,8 @@
+#
+# Date created: 2022-04-18
+# Author: aradclif
+#
+#
+############################################################################################
+# using Documenter, VectorizedReduction
+# makedocs(sitename=)
