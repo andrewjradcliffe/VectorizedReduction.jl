71724e28e5f0feecaf3ed826095cc52f0cf3cffa
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-06-09 13:37:27 -0700

    Add various tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -115,4 +115,116 @@ using Test
         val2, ind2 = vfindmax(h, B1, B2, B3, dims=(2,4))
         @test ind1 == ind2 && val1 ≈ val2
     end
+    ############################################################################################
+    @testset "from reducedim.jl tests" begin
+        A = [1.0 5.0 6.0;
+             5.0 2.0 4.0]
+        for (tup, rval, rind) in [((1,), [1.0 2.0 4.0], [CartesianIndex(1,1) CartesianIndex(2,2) CartesianIndex(2,3)]),
+                                  ((2,), reshape([1.0,2.0], 2, 1), reshape([CartesianIndex(1,1),CartesianIndex(2,2)], 2, 1)),
+                                  ((1,2), fill(1.0,1,1),fill(CartesianIndex(1,1),1,1))]
+            @test vfindmin1(A, dims=tup) == (rval, rind)
+            @test isequal(vvminimum(A, dims=tup), rval)
+        end
+
+        for (tup, rval, rind) in [((1,), [5.0 5.0 6.0], [CartesianIndex(2,1) CartesianIndex(1,2) CartesianIndex(1,3)]),
+                                  ((2,), reshape([6.0,5.0], 2, 1), reshape([CartesianIndex(1,3),CartesianIndex(2,1)], 2, 1)),
+                                  ((1,2), fill(6.0,1,1),fill(CartesianIndex(1,3),1,1))]
+            @test vfindmax1(A, dims=tup) == (rval, rind)
+            @test isequal(vvmaximum(A, dims=tup), rval)
+        end
+    end
+    @testset "findmin/findmax transformed arguments, numeric values" begin
+        A = [1.0 -5.0 -6.0;
+             -5.0 2.0 4.0]
+        TA = [((1,), [1.0 2.0 4.0], [CartesianIndex(1,1) CartesianIndex(2,2) CartesianIndex(2,3)]),
+              ((2,), reshape([1.0, 2.0], 2, 1), reshape([CartesianIndex(1,1), CartesianIndex(2,2)], 2, 1)),
+              ((1,2), fill(1.0,1,1), fill(CartesianIndex(1,1),1,1))]
+        TA2 = [((1,), [1.0 4.0 16.0], [CartesianIndex(1,1) CartesianIndex(2,2) CartesianIndex(2,3)]),
+               ((2,), reshape([1.0, 4.0], 2, 1), reshape([CartesianIndex(1,1), CartesianIndex(2,2)], 2, 1)),
+               ((1,2), fill(1.0,1,1), fill(CartesianIndex(1,1),1,1))]
+        TAc = [((1,), [0.28366218546322625 -0.4161468365471424 -0.6536436208636119], [CartesianIndex(2,1) CartesianIndex(2,2) CartesianIndex(2,3)]),
+               ((2,), reshape([0.28366218546322625, -0.6536436208636119], 2, 1), reshape([CartesianIndex(1,2), CartesianIndex(2,3)], 2, 1)),
+               ((1,2), fill(-0.6536436208636119,1,1), fill(CartesianIndex(2,3),1,1))]
+        for (f, At) in ((abs, TA), (abs2, TA2), (cos, TAc))
+            A′ = map(f, A)
+            for (tup, rval, rind) in At
+                (rval′, rind′) = vfindmin1(f, A, dims=tup)
+                @test all(rval′ .≈ rval)
+                @test rind′ == rind
+                (rval′′, rind′′) = vfindmin1(A′, dims=tup)
+                @test all(rval′ .≈ rval′′)
+                @test rind′ == rind′′
+            end
+        end
+
+        TA = [((1,), [5.0 5.0 6.0], [CartesianIndex(2,1) CartesianIndex(1,2) CartesianIndex(1,3)]),
+              ((2,), reshape([6.0,5.0], 2, 1), reshape([CartesianIndex(1,3), CartesianIndex(2,1)], 2, 1)),
+              ((1,2), fill(6.0,1,1),fill(CartesianIndex(1,3),1,1))]
+        TA2 = [((1,), [25.0 25.0 36.0], [CartesianIndex(2,1) CartesianIndex(1,2) CartesianIndex(1,3)]),
+               ((2,), reshape([36.0, 25.0], 2, 1), reshape([CartesianIndex(1,3), CartesianIndex(2,1)], 2, 1)),
+               ((1,2), fill(36.0,1,1), fill(CartesianIndex(1,3),1,1))]
+        TAc = [((1,), [0.5403023058681398 0.28366218546322625 0.960170286650366], [CartesianIndex(1,1) CartesianIndex(1,2) CartesianIndex(1,3)]),
+               ((2,), reshape([0.960170286650366, 0.28366218546322625], 2, 1), reshape([CartesianIndex(1,3), CartesianIndex(2,1)], 2, 1)),
+               ((1,2), fill(0.960170286650366,1,1), fill(CartesianIndex(1,3),1,1))]
+        for (f, At) in ((abs, TA), (abs2, TA2), (cos, TAc))
+            A′ = map(f, A)
+            for (tup, rval, rind) in At
+                (rval′, rind′) = vfindmax1(f, A, dims=tup)
+                @test all(rval′ .≈ rval)
+                @test rind′ == rind
+                (rval′′, rind′′) = vfindmax1(A′, dims=tup)
+                @test all(rval′ .≈ rval′′)
+                @test rind′ == rind′′
+            end
+        end
+    end
+
+    @testset "NaN in findmin/findmax/minimum/maximum" begin
+        A = [1.0 NaN 6.0;
+             NaN 2.0 4.0]
+        A′ = [-1.0 NaN -6.0;
+              NaN -2.0 4.0]
+        for (tup, rval, rind) in [((1,), [1.0 2.0 4.0], [CartesianIndex(1,1) CartesianIndex(2,2) CartesianIndex(2,3)]),
+                                  ((2,), reshape([1.0, 2.0], 2, 1), reshape([CartesianIndex(1,1),CartesianIndex(2,2)], 2, 1)),
+                                  ((1,2), fill(1.0,1,1),fill(CartesianIndex(1,1),1,1))]
+            @test isequal(vfindmin1(A, dims=tup), (rval, rind))
+            @test isequal(vfindmin1(abs, A′, dims=tup), (rval, rind))
+            @test isequal(vvminimum(A, dims=tup), rval)
+            @test isequal(vvminimum(abs, A′, dims=tup), rval)
+        end
+
+        for (tup, rval, rind) in [((1,), [1.0 2.0 6.0], [CartesianIndex(1,1) CartesianIndex(2,2) CartesianIndex(1,3)]),
+                                  ((2,), reshape([6.0, 4.0], 2, 1), reshape([CartesianIndex(1,3),CartesianIndex(2,3)], 2, 1)),
+                                  ((1,2), fill(6.0,1,1),fill(CartesianIndex(1,3),1,1))]
+            @test isequal(vfindmax1(A, dims=tup), (rval, rind))
+            @test isequal(vfindmax1(abs, A′, dims=tup), (rval, rind))
+            @test isequal(vvmaximum(A, dims=tup), rval)
+            @test isequal(vvmaximum(abs, A′, dims=tup), rval)
+        end
+    end
+
+    @testset "+/-Inf in findmin/findmax/minimum/maximum" begin
+        A = [Inf -Inf Inf  -Inf;
+             Inf  Inf -Inf -Inf]
+        A′ = [1 0 1 0;
+              1 1 0 0]
+        retinf(x::T) where {T} = ifelse(x == one(T), Inf, -Inf)
+        for (tup, rval, rind) in [((1,), [Inf -Inf -Inf -Inf], [CartesianIndex(1,1) CartesianIndex(1,2) CartesianIndex(2,3) CartesianIndex(1,4)]),
+                                  ((2,), reshape([-Inf -Inf], 2, 1), reshape([CartesianIndex(1,2),CartesianIndex(2,3)], 2, 1)),
+                                  ((1,2), fill(-Inf,1,1),fill(CartesianIndex(1,2),1,1))]
+            @test isequal(vfindmin1(A, dims=tup), (rval, rind))
+            @test isequal(vfindmin1(retinf, A′, dims=tup), (rval, rind))
+            @test isequal(vvminimum(A, dims=tup), rval)
+            @test isequal(vvminimum(retinf, A′, dims=tup), rval)
+        end
+
+        for (tup, rval, rind) in [((1,), [Inf Inf Inf -Inf], [CartesianIndex(1,1) CartesianIndex(2,2) CartesianIndex(1,3) CartesianIndex(1,4)]),
+                                  ((2,), reshape([Inf Inf], 2, 1), reshape([CartesianIndex(1,1),CartesianIndex(2,1)], 2, 1)),
+                                  ((1,2), fill(Inf,1,1),fill(CartesianIndex(1,1),1,1))]
+            @test isequal(vfindmax1(A, dims=tup), (rval, rind))
+            @test isequal(vfindmax1(retinf, A′, dims=tup), (rval, rind))
+            @test isequal(vvmaximum(A, dims=tup), rval)
+            @test isequal(vvmaximum(retinf, A′, dims=tup), rval)
+        end
+    end
 end
