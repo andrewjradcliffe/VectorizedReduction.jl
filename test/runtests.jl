f439f18faf1210436fb716c0d2a00b3358d88fc1
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-06-09 19:37:43 -0700

    Revise runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,18 @@
 using VectorizedReduction
 using Test
 
-@testset "VectorizedReduction.jl" begin
-    const tests = [
-        "vrspecials.jl"
-        "reduce.jl",
-        "reducedim.jl",
-        "treduce.jl",
-        "treducedim.jl"
-    ]
-    for t in tests
-        @testset "Test $t" begin
-            include(t)
-        end
+const tests = [
+    "vrspecials.jl",
+    "reduce.jl",
+    "reducedim.jl",
+    "treduce.jl",
+    "treducedim.jl"
+]
+
+# @testset "VectorizedReduction.jl" begin
+for t in tests
+    @testset "Test $t" begin
+        include(t)
     end
 end
+# end
