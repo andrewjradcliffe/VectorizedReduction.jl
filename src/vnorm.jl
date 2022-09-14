911921ba4071d749141c38089c817b90d589928a
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-09-13 22:13:10 -0700

    Clean and add various implementations

--- src/vnorm.jl
+++ src/vnorm.jl
@@ -7,68 +7,9 @@
 _denom(A, dims) = prod(d -> size(A, d), dims)
 _denom(A, ::Colon) = length(A)
 
-
-veuclidean(x, y; dims=:) = vmapreducethen((xᵢ, yᵢ) -> abs2(xᵢ - yᵢ) , +, √, x, y, dims=dims)
-vrmse(x, y; dims=:) = vmapreducethen((xᵢ, yᵢ) -> abs2(xᵢ - yᵢ) , +, √, x, y, dims=dims)
-
 _norm0(A, dims::NTuple{M, Int}) where {M} =
     fill!(similar(A, ntuple(d -> d ∈ dims ? 1 : size(A, d), Val(M))), _denom(A, dims))
 _norm0(A, ::Colon) = float(length(A))
-
-# Base.@constprop :aggressive function _vnorm(A, p::T, dims) where {T<:AbstractFloat}
-#     if p === one(T)
-#         vvsum(abs, A, dims=dims)
-#     elseif p === 2one(T)
-#         vmapreducethen(abs2, +, √, A, dims=dims)
-#     elseif p === typemax(T)
-#         vvmaximum(abs, A, dims=dims)
-#     elseif p === typemin(T)
-#         vvminimum(abs, A, dims=dims)
-#     else
-#         vmapreducethen(x -> exp(p * log(abs(x))), +, x -> exp(1/p * log(abs(x))), A, dims=dims)
-#     end
-# end
-# function _vnorm(A, p::Integer, dims)
-#     if p == 1
-#         vvsum(abs, A, dims=dims)
-#     elseif p == 2
-#         vmapreducethen(abs2, +, √, A, dims=dims)
-#     else
-#         vmapreducethen(x -> abs(x)^p, +, x -> x^(1/p), A, dims=dims)
-#     end
-# end
-# vnorm(A, p::Real=2; dims=:) = p == 0 ? _norm0(A, dims) : _vnorm(A, p, dims)
-
-for prefix ∈ (:v, :vt)
-    local norm = Symbol(prefix, :norm)
-    local _norm = Symbol(:_, norm)
-    local _norm_impl = Symbol(_norm, :_impl)
-    local f2 = Symbol(prefix, :mapreducethen)
-    local f1 = prefix === :v ? :vvsum : :vtsum
-    local finf = prefix === :v ? :vvmaximum : :vtmaximum
-    local fninf = prefix === :v ? :vvminimum : :vtminimum
-    @eval $_norm(A, p::Rational{T}, dims) where {T} = $_norm(A, float(p), dims)
-    @eval Base.@constprop :aggressive function $_norm(A, p, dims)
-        if p == 2
-            $f2(abs2, +, √, A, dims=dims)
-        elseif p == 1
-            $f1(abs, A, dims=dims)
-        elseif p == Inf
-            $finf(abs, A, dims=dims)
-        elseif p == -Inf
-            $fninf(abs, A, dims=dims)
-        else
-            $_norm_impl(A, p, dims)
-        end
-    end
-
-    @eval $_norm_impl(A, p::T, dims) where {T<:Integer} =
-        $f2(x -> abs(x)^p, +, x -> x^(one(T)/p), A, dims=dims)
-    @eval $_norm_impl(A, p::T, dims) where {T<:AbstractFloat} =
-        $f2(x -> exp(p * log(abs(x))), +, x -> exp(one(T)/p * log(abs(x))), A, dims=dims)
-
-    @eval $norm(A, p::Real=2; dims=:) = p == 0 ? _norm0(A, dims) : $_norm(A, p, dims)
-end
 
 _vnorm(A, p::Rational{T}, dims) where {T} = _vnorm(A, float(p), dims)
 Base.@constprop :aggressive function _vnorm(A, p, dims)
@@ -84,7 +25,6 @@ Base.@constprop :aggressive function _vnorm(A, p, dims)
         _vnorm_impl(A, p, dims)
     end
 end
-
 _vnorm_impl(A, p::T, dims) where {T<:Integer} =
     vmapreducethen(x -> abs(x)^p, +, x -> x^(one(T)/p), A, dims=dims)
 _vnorm_impl(A, p::T, dims) where {T<:AbstractFloat} =
@@ -106,7 +46,6 @@ Base.@constprop :aggressive function _vtnorm(A, p, dims)
         _vtnorm_impl(A, p, dims)
     end
 end
-
 _vtnorm_impl(A, p::T, dims) where {T<:Integer} =
     vtmapreducethen(x -> abs(x)^p, +, x -> x^(one(T)/p), A, dims=dims)
 _vtnorm_impl(A, p::T, dims) where {T<:AbstractFloat} =
@@ -114,13 +53,41 @@ _vtnorm_impl(A, p::T, dims) where {T<:AbstractFloat} =
 
 vtnorm(A, p::Real=2; dims=:) = p == 0 ? _norm0(A, dims) : _vtnorm(A, p, dims)
 
+# Distances
+veuclidean(x, y; dims=:) = vmapreducethen((xᵢ, yᵢ) -> abs2(xᵢ - yᵢ) , +, √, x, y, dims=dims)
+vteuclidean(x, y; dims=:) = vtmapreducethen((xᵢ, yᵢ) -> abs2(xᵢ - yᵢ) , +, √, x, y, dims=dims)
+vmanhattan(x, y; dims=:) = vvmapreduce((xᵢ, yᵢ) -> abs(xᵢ - yᵢ), +, x, y, dims=dims)
+vtmanhattan(x, y; dims=:) = vtmapreduce((xᵢ, yᵢ) -> abs(xᵢ - yᵢ), +, x, y, dims=dims)
 
+function vmse(x, y; dims=:)
+    c = 1 / _denom(x, dims)
+    vmapreducethen((xᵢ, yᵢ) -> abs2(xᵢ - yᵢ) , +, z -> c * z, x, y, dims=dims)
+end
+function vtmse(x, y; dims=:)
+    c = 1 / _denom(x, dims)
+    vtmapreducethen((xᵢ, yᵢ) -> abs2(xᵢ - yᵢ) , +, z -> c * z, x, y, dims=dims)
+end
+function vrmse(x, y; dims=:)
+    c = 1 / _denom(x, dims)
+    vmapreducethen((xᵢ, yᵢ) -> abs2(xᵢ - yᵢ) , +, z -> √(c * z), x, y, dims=dims)
+end
+function vtrmse(x, y; dims=:)
+    c = 1 / _denom(x, dims)
+    vtmapreducethen((xᵢ, yᵢ) -> abs2(xᵢ - yᵢ) , +, z -> √(c * z), x, y, dims=dims)
+end
 
-_vmean(f, g, A, dims) = vmapreducethen(f, +, g, A, dims=dims)
-vmean(f, A; dims=:) = (c = 1 / _denom(A, dims); _vmean(f, x -> c * x, A, dims))
+function vmean(f, A; dims=:)
+    c = 1 / _denom(A, dims)
+    vmapreducethen(f, +, x -> c * x, A, dims=dims)
+end
 vmean(A; dims=:) = vmean(identity, A, dims=dims)
 
+function vtmean(f, A; dims=:)
+    c = 1 / _denom(A, dims)
+    vtmapreducethen(f, +, x -> c * x, A, dims=dims)
+end
+vtmean(A; dims=:) = vtmean(identity, A, dims=dims)
 
-
-euclidean(x, y; dims=:) = .√mapreduce((xᵢ, yᵢ) -> abs2(xᵢ - yᵢ) , +, x, y, dims=dims)
+# Naturally, faster than the overflow/underflow-safe logsumexp, but if one can tolerate it...
 vlse(A; dims=:) = vmapreducethen(exp, +, log, A, dims=dims)
+vtlse(A; dims=:) = vtmapreducethen(exp, +, log, A, dims=dims)
