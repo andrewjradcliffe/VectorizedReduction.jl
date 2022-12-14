8371f8aef4177aed5439c011343a6db15acc3e0a
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-12-14 12:58:23 -0800

    Update doc

--- src/vdistance.jl
+++ src/vdistance.jl
@@ -48,7 +48,7 @@ _vtminkowski(x, y, p::Rational{T}, dims=:) where {T} = _vtminkowski(x, y, float(
     vtminkowski(x::AbstractArray, y::AbstractArray, p::Real; dims=:)
 
 Compute the Minkowski distance between the vectors corresponding to the slices along the
-dimensions `dims`.
+dimensions `dims`. Threaded.
 
 `p` can assume any numeric value (even though not all values produce a
 mathematically valid vector norm). `vtminkowski(x, y, Inf)` returns the largest value in
